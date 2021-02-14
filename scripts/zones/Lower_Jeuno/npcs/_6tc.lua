223a9f408d9b49ae489885779604944c08be2492
Aurasplash <aurasplash@gmail.com>
2021-02-13 19:02:00 -0700

    Black Belt
    Keep brown belt when getting black belt, for releveling purposes.

--- scripts/zones/Lower_Jeuno/npcs/_6tc.lua
+++ scripts/zones/Lower_Jeuno/npcs/_6tc.lua
@@ -76,7 +76,7 @@ entity.onEventFinish = function(player, csid, option)
             player:messageSpecial(ID.text.ITEM_OBTAINED, 13186)
             player:setCharVar("BeatAroundTheBushin", 0)
             player:addFame(NORG, 125)
-            player:tradeComplete()
+            -- player:tradeComplete()
             player:completeQuest(tpz.quest.log_id.JEUNO, tpz.quest.id.jeuno.BEAT_AROUND_THE_BUSHIN)
         end
     end
