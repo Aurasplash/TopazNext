c5e2ef962777aace5687c574a9549b6dde8c6057
Aurasplash <aurasplash@gmail.com>
2021-02-14 11:47:35 -0700

    EXP Chain
    linear exp chain increases except for 5+

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -3606,22 +3606,22 @@ namespace charutils
                                     exp *= 1.0f;
                                     break;
                                 case 1:
-                                    exp *= 1.2f;
+                                    exp *= 1.15f;
                                     break;
                                 case 2:
-                                    exp *= 1.25f;
+                                    exp *= 1.30f;
                                     break;
                                 case 3:
-                                    exp *= 1.3f;
+                                    exp *= 1.45f;
                                     break;
                                 case 4:
-                                    exp *= 1.4f;
+                                    exp *= 1.60f;
                                     break;
                                 case 5:
-                                    exp *= 1.5f;
+                                    exp *= 1.75f;
                                     break;
                                 default:
-                                    exp *= 1.55f;
+                                    exp *= 2.00f;
                                     break;
                             }
                         }
